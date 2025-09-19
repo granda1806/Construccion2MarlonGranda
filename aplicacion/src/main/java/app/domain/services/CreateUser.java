07e7de2295dffdbc968d863a3f627d9ac4a79280
granda1806 <granda1806@gmail.com>
2025-09-18 20:07:48 -0500

    actualizacion crear user

--- aplicacion/src/main/java/app/domain/services/CreateUser.java
+++ aplicacion/src/main/java/app/domain/services/CreateUser.java
@@ -1,37 +1,40 @@
 package app.domain.services;
 
 import app.domain.model.User;
-import app.domain.model.enums.Role;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import app.domain.ports.UserPortIn;
+import app.domain.ports.UserPortOut;
+import org.springframework.stereotype.Service;
 
 @Service
-public class CreateUser
-{
-    
-    @Autowired
-    private UserPortIn userPort;
-    
-    public void create(User user) throws Exception
-    {
-        
-        if (userPort.findByDocument(user) != null)
-        {
-            
-            System.out.println("Ya hay un usuario registrado con este documento.");
-            
-        }
-        
-        if (userPort.findByName(user) != null)
-        {
-            
-            System.out.println("Ya hay una persona registrada con este nombre de usuario");
-            
-        }
-        
-        userPort.save(user);
-        
+public class CreateUser implements UserPortIn {
+
+    private final UserPortOut userPortOut;
+
+    public CreateUser(UserPortOut userPortOut) {
+        this.userPortOut = userPortOut;
     }
-    
+
+    @Override
+    public User findByDocument(User user) throws Exception {
+        return userPortOut.findByDocument(user);
+    }
+
+    @Override
+    public User findByName(User user) throws Exception {
+        return userPortOut.findByName(user);
+    }
+
+    @Override
+    public void save(User user) throws Exception {
+        if (userPortOut.findByDocument(user) != null) {
+            System.out.println("Ya hay un usuario registrado con este documento.");
+            return;
+        }
+        if (userPortOut.findByName(user) != null) {
+            System.out.println("Ya hay una persona registrada con este nombre de usuario");
+            return;
+        }
+        userPortOut.save(user);
+    }
 }
+
